2c93ba8aec9713b7f0979b1a6afdf7cb249d5727
Dmitry Terentev <zabey52@gmail.com>
2022-12-14 21:39:37 +0300

    v1

--- 08_01/Function.cpp
+++ 08_01/Function.cpp
@@ -1,30 +1,11 @@
-// 08_Задача 1. Простое исключение
-//
-//#include <iostream>
-//
-//class DivisionByZeroException : public std::exception
-//{
-//public:
-//	const char* what() const override { return "Попытка деления на 0"; }
-//};
-//
-//float divide(float dividend, float divisor)
-//{
-//	if (divisor == 0) throw DivisionByZeroException();
-//	return dividend / divisor;
-//}
-//
-//int main(int argc, char** argv)
-//{
-//	setlocale(LC_ALL, "Russian");
-//
-//	float a = 5.6, b = 1.3, c = 0;
-//
-//	try
-//	{
-//		c = divide(a, b);
-//		std::cout << a << " / " << b << " = " << c << std::endl;
-//	}
-//	catch (const DivisionByZeroException& ex) { std::cout << ex.what() << std::endl; }
-//	catch (...) { std::cout << "Неизвестная ошибка" << std::endl; }
-//}
+#include "Function.h"
+
+int function(std::string anyWord, int forbiddenLength)
+{
+    int badLength = 1;
+
+    if (forbiddenLength == anyWord.length()) throw badLength;
+
+    { return static_cast<int>(anyWord.length()); }
+
+}
